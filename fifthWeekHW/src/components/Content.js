cc5b0a3510e7a6aca244d487787e2aea60689f6b
yılmaz canpolat <71183671+ylmzcanpolat@users.noreply.github.com>
2021-11-27 03:05:01 +0300

    Add files via upload

---
+++ fifthWeekHW/src/components/Content.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import TodoList from './TodoList'
+import Footer from './Footer'
+
+function Content({ todoList, todoFilter, filterSet, todosSet }) {
+    return (
+        <>
+            <section className="main">
+                <input className="toggle-all" type="checkbox" />
+                <TodoList todos={todoList} todosSet={todosSet} />
+            </section>
+            <Footer todoFilter={todoFilter} filterSet={filterSet} todos={todoList} todosSet={todosSet} />
+        </>
+    )
+}
+
+export default Content
